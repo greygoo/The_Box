1d8ec646b6328067a7d4d4e90a453b0622db7eba
Christian Hueller <chuller@suse.de>
2015-07-15 12:33:20 +0200

    add servo_dance function.

--- The_Box.ino
+++ The_Box.ino
@@ -1,44 +1,45 @@
 #include <Servo.h>
 
-const int ledPin = 13;
 const int ledRed = 7;
 const int ledGreen = 8;
 String incoming = "";
-Servo myservo;
+Servo boxServo;
 
 int pos = 0;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
   pinMode(ledGreen, OUTPUT);
   pinMode(ledRed, OUTPUT);
   
-  myservo.attach(9);
+  boxServo.attach(9);
   Serial.begin(9600);
 }
 
-void loop() {
-  if (Serial.available() > 0) {          
-    char in = (char)Serial.read();
-    if (in == 'A') {
-      digitalWrite(ledPin, HIGH);
-      digitalWrite(ledRed, LOW);
-      digitalWrite(ledGreen, HIGH);
+void dance_servo() {
       for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees
       {                                  // in steps of 1 degree
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
         delay(15);                       // waits 15ms for the servo to reach the position
       }
       for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees
-      {                                
+      {
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
       delay(15);
       }
+}
+
+void loop() {
+  if (Serial.available() > 0) {          
+    char in = (char)Serial.read();
+    if (in == 'A') {
+      digitalWrite(ledRed, LOW);
+      digitalWrite(ledGreen, HIGH);
+      dance_servo();
     }
     if (in == 'O') {
-      digitalWrite(ledPin, LOW);
       digitalWrite(ledRed, HIGH);
       digitalWrite(ledGreen, LOW);
+      dance_servo();
     }
   }
 }
