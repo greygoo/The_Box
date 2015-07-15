9ff800c49bb2f1830d127833366af4b21f6221ac
Christian Hueller <chuller@suse.de>
2015-07-15 13:19:09 +0200

    Add code for additional leds and make servo jitter.

--- The_Box.ino
+++ The_Box.ino
@@ -1,30 +1,49 @@
 #include <Servo.h>
 
-const int ledRed = 7;
-const int ledGreen = 8;
-String incoming = "";
-Servo boxServo;
-
+const int ledWhite1 = 3;
+const int ledWhite2 = 4;
+const int ledPurple1 = 5;
+const int ledPurple2 = 6;
+const int ledBlue1 = 7;
+const int ledBlue2 = 8;
+const int ledRed1 = 9;
+const int ledRed2 = 10;
+const int ledGreen1 = 11;
+const int ledGreen2 = 12;
 int pos = 0;
 
+Servo boxServo;
+
 void setup() {
-  pinMode(ledGreen, OUTPUT);
-  pinMode(ledRed, OUTPUT);
+  pinMode(ledGreen1, OUTPUT);
+  pinMode(ledGreen2, OUTPUT);
+  pinMode(ledRed1, OUTPUT);
+  pinMode(ledRed2, OUTPUT);
+  pinMode(ledPurple1, OUTPUT);
+  pinMode(ledPurple2, OUTPUT);
+  pinMode(ledWhite1, OUTPUT);
+  pinMode(ledWhite2, OUTPUT);
+  pinMode(ledBlue1, OUTPUT);
+  pinMode(ledBlue2, OUTPUT);
   
-  boxServo.attach(9);
+  boxServo.attach(13);
   Serial.begin(9600);
 }
 
+int jitter() {
+  return random(0,10) - 5;
+}
+
 void dance_servo() {
-      for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees
-      {                                  // in steps of 1 degree
-        myservo.write(pos);              // tell servo to go to position in variable 'pos'
-        delay(15);                       // waits 15ms for the servo to reach the position
-      }
-      for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees
+      for(pos = 0; pos < 180; pos += 1)
       {
-        myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);
+        boxServo.write(pos + jitter());
+        delay(15 + jitter());
+      }
+      for(pos = 180; pos>=1; pos-=1)
+      {
+        boxServo.write(pos + jitter());
+        delay(15 + jitter());
       }
 }
 
@@ -32,13 +51,13 @@ void loop() {
   if (Serial.available() > 0) {          
     char in = (char)Serial.read();
     if (in == 'A') {
-      digitalWrite(ledRed, LOW);
-      digitalWrite(ledGreen, HIGH);
+      digitalWrite(ledRed1, LOW);
+      digitalWrite(ledGreen1, HIGH);
       dance_servo();
     }
     if (in == 'O') {
-      digitalWrite(ledRed, HIGH);
-      digitalWrite(ledGreen, LOW);
+      digitalWrite(ledRed1, HIGH);
+      digitalWrite(ledGreen1, LOW);
       dance_servo();
     }
   }
