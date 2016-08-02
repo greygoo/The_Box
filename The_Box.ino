879fea0d695879591e4587860a6aeeffba6b3313
chuller <chuller@suse.de>
2016-08-02 15:58:53 +0200

    Clean up, remove old code.

--- The_Box.ino
+++ The_Box.ino
@@ -1,31 +1,33 @@
-#include <Servo.h>
+#include <ServoTimer2.h>
+#include <LED_Switch.h>
+#include <Arduino.h>
 #include <TrueRandom.h>
+#include <Servo_Jump.h>
+#include <Flicker.h>
 
-const int ledFlicker2_blue = 5;
-const int ledFlicker2_white = 6;
-const int ledFlicker1_blue = 9;
-const int ledFlicker1_white = 10;
+const int ledFlicker2_blue = 9;
+const int ledFlicker2_white = 10;
+const int ledFlicker1_blue = 5;
+const int ledFlicker1_white = 6;
 const int ledBacklight_yellow = 11;
 const int ledMain_green = 8;
 const int ledMain_yellow = 12;
 const int ledMain_red = 13;
-const int ledFlask1_green = A5;
-const int ledFlask1_red = A4;
-const int ledFlask2_green = A3;
-const int ledFlask2_red = A2;
-const int ledFlask3_green = A1;
-const int ledFlask3_red = A0;
-int pos = 0;
+int flickerDuration = 8000;
+int GaugeDuration = 10000;
+int LED_timeout = 9000;
 
-Servo boxServo;
+Flicker Flicker1(ledFlicker1_blue, 10, 40, 10, 250,  flickerDuration);
+Flicker Flicker2(ledFlicker1_white, 10, 40, 10, 250, flickerDuration);
+Flicker Flicker3(ledFlicker2_blue, 1, 10, 10, 250, flickerDuration);
+Flicker Flicker4(ledFlicker2_white, 1, 10, 10, 250, flickerDuration);
+
+Servo_Jump Gauge(200, 0, 180, GaugeDuration);
+
+LED_Switch LED_red(1, 8000);
+LED_Switch LED_green(0, 8000);
 
 void setup() {
-  pinMode(ledFlask1_red, OUTPUT);
-  pinMode(ledFlask2_red, OUTPUT);
-  pinMode(ledFlask3_red, OUTPUT);
-  pinMode(ledFlask1_green, OUTPUT);
-  pinMode(ledFlask2_green, OUTPUT);
-  pinMode(ledFlask3_green, OUTPUT);
   pinMode(ledBacklight_yellow, OUTPUT);
   pinMode(ledFlicker1_blue, OUTPUT);
   pinMode(ledFlicker2_blue, OUTPUT);
@@ -37,84 +39,52 @@ void setup() {
   
   digitalWrite(ledBacklight_yellow, HIGH);
 
-  boxServo.attach(3);
+  Gauge.Attach(3);
   Serial.begin(9600);
 }
 
-int jitter() {
-  return random(0,10) - 5;
-}
-
 void dance_servo() {
-      for(pos = 0; pos < 180; pos += 1)
-      {
-       // if (pos >= 180) { pos = 179; };
-        boxServo.write(pos);
-        // delay(15 + jitter());
-        delay(5);
-      }
-      for(pos = 180; pos>=1; pos-= 1)
-      {
-       // if (pos <= 0) { pos = 1; };
-        boxServo.write(pos);
-        // delay(15 + jitter());
-        delay(5);
-      }
+  Gauge.Enable();
 }
 
-void flask_light(int release) {
-      if (release == 1) {
-        digitalWrite(ledFlask1_red, LOW);
-        digitalWrite(ledFlask2_red, LOW);
-        digitalWrite(ledFlask3_red, LOW);
-        digitalWrite(ledFlask1_green, HIGH);
-        digitalWrite(ledFlask2_green, HIGH);
-        digitalWrite(ledFlask3_green, HIGH);
-      } else if (release == 0) {
-        digitalWrite(ledFlask1_red, HIGH);
-        digitalWrite(ledFlask2_red, HIGH);
-        digitalWrite(ledFlask3_red, HIGH);
-        digitalWrite(ledFlask1_green, LOW);
-        digitalWrite(ledFlask2_green, LOW);
-        digitalWrite(ledFlask3_green, LOW);
-      } else {
-        digitalWrite(ledFlask1_red, HIGH);
-        digitalWrite(ledFlask2_red, HIGH);
-        digitalWrite(ledFlask3_red, HIGH);
-        digitalWrite(ledFlask1_green, HIGH);
-        digitalWrite(ledFlask2_green, HIGH);
-        digitalWrite(ledFlask3_green, HIGH);
-      }       
-}
+void test(){
+  Serial.print("Hello World\n");
+}  
 
 void flicker() {
-      for(int i=0; i < 300 ; i++)
-      {
-        analogWrite(ledFlicker1_white, TrueRandom.random(0,155));
-        analogWrite(ledFlicker1_blue, TrueRandom.random(0,155));
-        analogWrite(ledFlicker2_white, TrueRandom.random(0,155));
-        analogWrite(ledFlicker2_blue, TrueRandom.random(0,155));
-        delay(TrueRandom.random(10,40));
-      }
-      digitalWrite(ledFlicker1_white, LOW);
-      digitalWrite(ledFlicker1_blue, LOW);
-      digitalWrite(ledFlicker2_white, LOW);
-      digitalWrite(ledFlicker2_blue, LOW);
+  Flicker1.Enable();
+  Flicker2.Enable();
+  Flicker3.Enable();
+  Flicker4.Enable();
 }
 
+void update_all() {
+  Flicker1.Update();
+  Flicker2.Update();
+  Flicker3.Update();
+  Flicker4.Update();
+
+  Gauge.Update();
+  
+  LED_green.Update();
+  LED_red.Update();
+}
 
 void loop() {
+  update_all();
   if (Serial.available() > 0) {          
     char in = (char)Serial.read();
     if (in == 'A') {
       flicker();
       dance_servo();
-      flask_light(1);
+      Gauge.JumpTo(0);
+      LED_red.Enable();
     }
     if (in == 'O') {
       flicker();
       dance_servo();
-      flask_light(0);
+      Gauge.JumpTo(180);
+      LED_green.Enable();
     }
   }
 }
